4aaea86da9f4cbd942e50f14b9681d968e134103
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-06 22:34:47 +0000

    Added UI Components

---
+++ resources/views/components/green-button.blade.php
@@ -0,0 +1,3 @@
+<button
+    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-500 focus:bg-green-500 active:bg-green-800']) }}>
+    {{ $slot }} </button>
