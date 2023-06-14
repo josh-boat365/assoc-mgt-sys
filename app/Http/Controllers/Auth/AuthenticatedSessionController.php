7184055fbc645211a0496bf6833c72b20cf60578
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-14 14:27:27 +0000

    Updated Auth Session Controller

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -25,12 +25,13 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        // $request->checkValidationForUsernameOrEmail();
+
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        // return redirect()->intended(RouteServiceProvider::HOME);
+         return redirect()->route('dashboard');
     }
 
     /**
