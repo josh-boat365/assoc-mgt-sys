f2fc80c1863175767e71fffc1c25dda31dcee0c0
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-14 14:34:45 +0000

    update: added flowbite, script to greet users based on time of the day

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,19 @@
 import './bootstrap';
+import 'flowbite';
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+//dashboard - greetings
+let greetings = document.getElementById('greetings');
+
+let timeNow = new Date().getHours();
+
+let greet = timeNow >= 5 && timeNow < 12 ? "Good Morning!"
+    : timeNow >= 12 && timeNow < 18 ? "Good Afteroon!"
+        : "Good Evening!";
+
+greetings.innerHTML = greet;
