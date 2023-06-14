9edb84e61b8f2cc268afd28ac3470f54f1de089a
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-14 14:42:09 +0000

    update: restructure

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,11 +10,15 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <!-- Scripts --> @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 bg1">
+        <x-auth-session-status class="mb-5" :status="session('status')" />
+        <x-success-message class="mb-5" :success="session('success')" />
+        <x-error-message class="mb-5" :fail="session('fail')" />
         <div>
             <a href="/">
                 <img class="ams-logo" src="{{ asset('imgs/ams-logo-full.png') }}" alt="AMS Logo">
@@ -23,6 +27,8 @@
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"> {{ $slot }}
         </div>
     </div>
+    <!-- Flowbite- JS  -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 </body>
 
 </html>
