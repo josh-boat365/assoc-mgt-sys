5c2f7b63c77bcd240404108fa5ab1137b9beab12
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-14 20:05:01 +0000

    update: added settings tab

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -8,11 +8,13 @@
     <div class="mt-5 md:container md:mx-auto">
         <div class="text-center">
             <span class=" text-[1.6rem] font-bold text-gray-800 " id="greetings"></span>
-            @forelse ($AuthUser as $user )
-            <span class="text-[1.6rem]">{{$user->firstname}} {{$user->surname}}</span>
-            @empty
+            @empty(Auth::user()->firstname && Auth::user()->surname)
             <span class="text-[1.6rem]">{{Auth::user()->username}}</span>
-            @endforelse
+            @else
+            <span class="text-[1.6rem]">{{Auth::user()->firstname}} {{Auth::user()->surname}}</span>
+            @endempty
+
+
             <p>{{$text}}</p>
         </div>
 
@@ -21,11 +23,11 @@
         <div class="py-12">
             <div class="max-w-5xl md:mx-auto sm:px-6 lg:px-8">
                 <div class="flex flex-wrap justify-center p-3 gap-5">
-                    <x-card href="{{ route('profile.edit')}}" class="w-[12rem] text-center border-t-4 rounded border-sky-700">
+                    <x-card href="{{ route('profile.view')}}" class="w-[12rem] text-center border-t-4 rounded border-sky-700">
                         <i class="fa-solid fa-id-badge fa-2xxl"></i>
                         <p class="text-[1.5rem] font-semi-bold">Profile</p>
                     </x-card>
-                    <x-card href="#" class="w-[12rem] text-center border-t-4 rounded border-lime-700">
+                    <x-card href="#" class="w-[12rem] text-center border-t-4 rounded border-yellow-400">
                         <i class="fa-solid fa-money-bill fa-2xxl"></i>
                         <p class="text-[1.5rem] font-semi-bold">Dues</p>
                     </x-card>
@@ -41,13 +43,19 @@
                         <i class="fa-solid fa-book-open-reader fa-2xxl"></i>
                         <p class="text-[1.5rem] font-semi-bold">Resources</p>
                     </x-card>
-                    <x-card href="#" class="w-[12rem] text-center border-t-4 rounded border-yellow-400">
+                    <x-card href="#" class="w-[12rem] text-center border-t-4 rounded  border-lime-700">
                         <i class="fa-solid fa-comments fa-2xxl"></i>
                         <p class="text-[1.5rem] font-semi-bold">Chats</p>
+                    </x-card>
+                    <x-card href="{{route('profile.edit')}}" class="w-[12rem] text-center border-t-4 rounded border-teal-600">
+                        <i class="fa-solid fa-gear fa-2xxl"></i>
+                        <p class="text-[1.5rem] font-semi-bold">Settings</p>
                     </x-card>
                 </div>
             </div>
         </div>
+
+
 
 
 
