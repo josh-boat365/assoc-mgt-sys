8bf09a7d7b9a3d92675df03eaf866ec54bd3f7a7
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-14 20:07:02 +0000

    update: restructure

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -16,88 +16,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="w-full">
-                    <section>
-                        <header>
-                            <h2 class="text-lg font-medium text-gray-900">
-                                {{ __('Profile Information') }}
-                            </h2>
+                    @include('profile.partials.update-profile-information-form')
+                </div>
+            </div>
 
-                            <p class="mt-1 text-sm text-gray-600">
-                                {{ __("Update your account's profile information and email address.") }}
-                            </p>
-                        </header>
-
-                        <form id="send-verification" method="post" action="{{ route('verification.send') }}">
-                            @csrf
-                        </form>
-
-                        <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
-                            @csrf
-                            @method('patch')
-
-                            <div class="w-[20rem]">
-                                <x-input-label for="firstname" :value="__('Association Number')" />
-                                <x-text-input id="bar_number" name="bar_number" type="text" class="mt-1 block w-full" :value="old('bar_number', $user->bar_number)" autofocus autocomplete="bar_number" />
-                                <x-input-error class="mt-2" :messages="$errors->get('bar_number')" />
-                            </div>
-
-                            <div class="flex res:flex-wrap gap-5 ">
-                                <div class="w-[20rem]">
-                                    <x-input-label for="firstname" :value="__('Firstname')" />
-                                    <x-text-input id="firstname" name="firstname" type="text" class="mt-1 block w-full" :value="old('firstname', $user->firstname)" autofocus autocomplete="surname" />
-                                    <x-input-error class="mt-2" :messages="$errors->get('firstname')" />
-                                </div>
-                                <div class="w-[20rem]">
-                                    <x-input-label for="surname" :value="__('Surname')" />
-                                    <x-text-input id="surname" name="surname" type="text" class="mt-1 block w-full" :value="old('surname', $user->surname)" autofocus autocomplete="surname" />
-                                    <x-input-error class="mt-2" :messages="$errors->get('surname')" />
-                                </div>
-                                <div class="w-[20rem]">
-
-                                    <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
-                                    <select id="countries" class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={{old('gender','$user->gender')}} autofocus autocomplete="gender">
-                                        <option selected>---- Choose gender ----</option>
-                                        <option value="M">Male</option>
-                                        <option value="F">Female</option>
-                                    </select>
-                                    <x-input-error class="mt-2" :messages="$errors->get('gender')" />
-                                </div>
-                            </div>
-
-                            <div>
-                                <x-input-label for="email" :value="__('Email')" />
-                                <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
-                                <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-                                @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                                <div>
-                                    <p class="text-sm mt-2 text-gray-800">
-                                        {{ __('Your email address is unverified.') }}
-
-                                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                            {{ __('Click here to re-send the verification email.') }}
-                                        </button>
-                                    </p>
-
-                                    @if (session('status') === 'verification-link-sent')
-                                    <p class="mt-2 font-medium text-sm text-green-600">
-                                        {{ __('A new verification link has been sent to your email address.') }}
-                                    </p>
-                                    @endif
-                                </div>
-                                @endif
-                            </div>
-
-                            <div class="flex items-center gap-4">
-                                <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-                                @if (session('status') === 'profile-updated')
-                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-600">{{ __('Saved.') }}</p>
-                                @endif
-                            </div>
-                        </form>
-                    </section>
-
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.update-password-form')
                 </div>
             </div>
 
