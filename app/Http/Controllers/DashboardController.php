9c3dbafffa4a698b7b180392b49539b77139ec22
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-19 18:37:49 +0000

    update: added function for views: shop, resources, chats, receipts, dues

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,10 +8,11 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         $user_id = Auth::id();
-        $AuthUser = User::where('id',$user_id);
+        $AuthUser = User::where('id', $user_id);
         // dd($AuthUser);
         $text = "Kindly take note! You would have to pay your monthly dues to be able to have full access to this application.";
 
@@ -19,7 +20,41 @@ class DashboardController extends Controller
     }
 
 
-    public function profile_view(){
+    public function profile_view()
+    {
         return view('profile.profile');
+    }
+
+    public function dues_view()
+    {
+        return view('dashboard.dues');
+    }
+
+    public function conference_view()
+    {
+        return view('dashboard.conference');
+    }
+
+
+    public function resources_view()
+    {
+        return view('dashboard.resources');
+    }
+
+
+    public function chats_view()
+    {
+        return view('dashboard.chats');
+    }
+
+
+    public function shop_view()
+    {
+        return view('dashboard.shop');
+    }
+
+    public function dues_receipt_view()
+    {
+        return view('dashboard.dues-receipt');
     }
 }
