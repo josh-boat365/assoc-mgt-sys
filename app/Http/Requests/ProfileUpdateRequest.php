863f67a1bc37f486dc28dcd46fca0718a4aae7e9
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-19 18:38:59 +0000

    update

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -16,8 +16,24 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['string', 'max:255'],
+            'assoc_number' => ['integer', 'min:4'],
+            'profile_image' => ['image', 'mimes:png,jpg,jpeg', 'max:5048'],
+            'firstname' => ['string', 'max:255'],
+            'surname' => ['string', 'max:255'],
+            'username' => ['string', 'min:6', 'max:12', Rule::unique(User::class)->ignore($this->user()->id)],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'tin' => ['string', 'max:20'],
+            'gender' => ['string', 'max:255'],
+            'date_of_birth' => ['string', 'max:255'],
+            'region_of_company' => ['string', 'max:255'],
+            'area_of_interests' => ['string', 'max:255'],
+            'regions_of_company' => ['string', 'max:255'],
+            'company_name' => ['string', 'max:255'],
+            'company_address' => ['string', 'max:255'],
+            'primary_contact' => ['string', 'min:10', 'max:10'],
+            'secondary_contact' => ['string', 'min:10', 'max:10'],
+            'onboard_date' => ['string', 'max:255'],
+            'academic_qualification' => ['string', 'max:255'],
         ];
     }
 }
