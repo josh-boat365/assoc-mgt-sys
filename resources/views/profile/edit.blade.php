dd8501d00cf100e3f00e0ed98a0383cc02052d49
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-19 18:42:57 +0000

    update

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,12 +6,18 @@
     </x-slot>
 
 
-   <div class="w-[6rem] ml-[10rem] mt-5  text-center p-2 shadow rounded">
-     <a href="{{route('dashboard')}}">
-        <span><i class="fa-solid fa-arrow-right-from-bracket fa-rotate-180 "></i>&nbsp;<spanclear
-        >Back</span></span>
-    </a>
+    <div class="w-[6rem] ml-[10rem] mt-5  text-center p-2 shadow rounded">
+        <a href="{{route('dashboard')}}">
+            <span><i class="fa-solid fa-arrow-right-from-bracket fa-rotate-180 "></i>&nbsp;<spanclear>Back</span></span>
+        </a>
+    </div>
+
+   <div class="m-auto w-[50rem] mt-5">
+     <x-auth-session-status class="mb-5" :status="session('status')" />
+    <x-success-message class="mb-5" :success="session('success')" />
+    <x-error-message class="mb-5" :fail="session('fail')" />
    </div>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
