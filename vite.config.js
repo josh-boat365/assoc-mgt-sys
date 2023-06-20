d27ef19103c4410905de622b6f88b5d015d40d89
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-20 12:56:48 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'public/fonts/fontawesome/css/all.min.css'
             ],
             refresh: true,
         }),
