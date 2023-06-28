57bb9b4ff0b6ae214aac3136da7632d80bf9148b
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-28 00:13:50 +0000

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,19 +6,24 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name') }}</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <!-- Scripts -->
     @vite(['resources/css/app.css','public/fonts/fontawesome/css/all.min.css','resources/js/app.js'])
 
+    @livewireStyles
+    <!-- @powerGridStyles -->
+
 
 </head>
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
+        
         @include('layouts.navigation')
+
         <!-- Page Heading -->
         @if (isset($header)) <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8"> {{ $header }} </div>
@@ -32,6 +37,9 @@
     <!-- Flowbite- JS  -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/datepicker.min.js"></script>
+
+    @livewireScripts
+    <!-- @powerGridScripts -->
 
 </body>
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,7 +15,7 @@
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 bg1">
+    <div class="min-h-screen flex flex-col res:justify-center sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 bg1">
         <x-auth-session-status class="mb-5" :status="session('status')" />
         <x-success-message class="mb-5" :success="session('success')" />
         <x-error-message class="mb-5" :fail="session('fail')" />
