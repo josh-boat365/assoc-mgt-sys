0e26b442b6eee496f0e106e643eb7ae4a7bdf321
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-28 00:19:54 +0000

    update: added new pages to user dashboard, dues, chats, conference, resources, home, and shop

---
+++ resources/views/user/conference.blade.php
@@ -0,0 +1,138 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Conference') }}
+        </h2>
+    </x-slot>
+
+    <div class="w-[6rem] ml-[10rem] mt-5  text-center p-2 shadow rounded">
+        <a href="{{route('admin.home')}}">
+            <span><i class="fa-solid fa-arrow-right-from-bracket fa-rotate-180 "></i>&nbsp;<spanclear>Back</span></span>
+        </a>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h2 class="mt-2 text-[2rem] text-center">Conference Registration</h2>
+                    <h3 class="mb-4 text-center text-[0.8rem]">Note that you have to pay your dues in order to Register for Conference.
+                        Click on Event Title to read details about the event</h3>
+                    <div class="relative overflow-x-auto">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class=" py-3">
+                                        #
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Details
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Status
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <!-- Price -->
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <!-- <span class="sr-only">Edit</span> -->
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+                                    <th scope="row" class=" py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        2
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        2021/2022 Association Dues
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        paid
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                        <!-- <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> -->
+                                        <a href="{{route('dues.receipt.view')}}">
+                                            <x-primary-button class="ml-4"> {{ __('View receipt') }} </x-primary-button>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+                                    <th scope="row" class=" py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        1
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        2019/2020 Association Dues
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        not-paid
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                        <!-- <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> -->
+                                        <a href="#">
+                                            <x-green-button class="ml-4"> {{ __('Make Payment') }} </x-green-button>
+                                        </a>
+                                    </td>
+                                </tr>
+
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+                                    <th scope="row" class=" py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        3
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        2022/2023 Association Dues
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        not-paid
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                        <!-- <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> -->
+                                        <a href="{#">
+                                            <x-green-button class="ml-4"> {{ __('Make Payment') }} </x-green-button>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+                                    <th scope="row" class=" py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        4
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        2023/2024 Association Dues
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        not-paid
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                        <!-- <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> -->
+                                        <a href="{#">
+                                            <x-green-button class="ml-4"> {{ __('Make Payment') }} </x-green-button>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
+                                    <th scope="row" class=" py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        5
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        2022/2023 Association Dues
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        not-paid
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                        <!-- <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> -->
+                                        <a href="{#">
+                                            <x-green-button class="ml-4"> {{ __('Make Payment') }} </x-green-button>
+                                        </a>
+                                    </td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,91 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="mt-5 md:container md:mx-auto">
+        <div class="text-center">
+            <span class=" text-[1.6rem] font-bold text-gray-800 " id="greetings"></span>
+            @empty(Auth::user()->firstname && Auth::user()->surname)
+            <span class="text-[1.6rem]">{{Auth::user()->username}}</span>
+            @else
+            <span class="text-[1.6rem]">{{Auth::user()->firstname}} {{Auth::user()->surname}}</span>
+            @endempty
+
+
+            <p>{{$text}}</p>
+        </div>
+
+
+
+        <div class="py-12">
+            <div class="max-w-5xl md:mx-auto sm:px-6 lg:px-8">
+                <div class="flex flex-wrap justify-center p-3 gap-5">
+
+                    <x-card href="{{ route('profile.view')}}" class="w-[12rem] text-center border-t-4 rounded border-sky-700">
+                        <i class="fa-solid fa-id-badge fa-2xxl"></i>
+                        <p class="text-[0.8rem]">
+                            Click on profile tab to view your profile information.
+                        </p>
+                        <p class="text-[1.5rem] font-semi-bold">Profile</p>
+                    </x-card>
+
+                    <x-card href="{{route('dues.view')}}" class="w-[12rem] text-center border-t-4 rounded border-yellow-400">
+                        <i class="fa-solid fa-money-bill fa-2xxl"></i>
+                         <p class="text-[0.8rem]">
+                            Click on dues tab to pay your monthly dues. <span class="text-[0.7rem]" style="color: orange !important;">Payment ensures full access to this platform.</span>
+                        </p>
+                        <p class="text-[1.5rem] font-semi-bold">Dues</p>
+                    </x-card>
+
+                    <x-card href="{{route('conference.view')}}" class="w-[12rem] text-center border-t-4 rounded border-purple-600">
+                        <i class="fa-solid fa-chalkboard-user fa-2xxl"></i>
+                        <p class="text-[0.8rem]">
+                            Click on conference tab to view and pay for upcoming conferences.
+                        </p>
+                        <p class="text-[1.5rem] font-semi-bold">Conference</p>
+                    </x-card>
+
+                    <x-card href="{{route('shop.view')}}" class="w-[12rem] text-center border-t-4 rounded border-teal-600">
+                        <i class="fa-solid fa-shop fa-2xxl"></i>
+                        <p class="text-[0.8rem]">
+                            Click on shop tab to purchase association related items.
+                        </p>
+                        <p class="text-[1.5rem] font-semi-bold">Shop</p>
+                    </x-card>
+
+                    <x-card href="{{route('resources.view')}}" class="w-[12rem] text-center border-t-4 rounded border-rose-700">
+                        <i class="fa-solid fa-book-open-reader fa-2xxl"></i>
+                        <p class="text-[0.8rem]">
+                            Click on resources tab to access to conference related files and association related files.
+                        </p>
+                        <p class="text-[1.5rem] font-semi-bold">Resources</p>
+                    </x-card>
+
+                    <x-card href="{{route('chats.view')}}" class="w-[12rem] text-center border-t-4 rounded  border-lime-700">
+                        <i class="fa-solid fa-comments fa-2xxl"></i>
+                        <p class="text-[0.8rem]">
+                            Click on chats tab to message colleagues via email in your region.
+                        </p>
+                        <p class="text-[1.5rem] font-semi-bold">Chats</p>
+                    </x-card>
+
+                    <x-card href="{{route('profile.edit')}}" class="w-[12rem] text-center border-t-4 rounded border-teal-600">
+                        <i class="fa-solid fa-gear fa-2xxl"></i>
+                        <p class="text-[0.8rem]">
+                            Click on settings tab to update profile and password.
+                        </p>
+                        <p class="text-[1.5rem] font-semi-bold">Settings</p>
+                    </x-card>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+    </div>
+</x-app-layout>
