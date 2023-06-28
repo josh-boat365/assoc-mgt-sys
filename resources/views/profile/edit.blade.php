6a6a530bda65bbec29291cf07f503691418bd98f
josh-boat365 <nyarko.boateng20@outlook.com>
2023-06-28 00:16:07 +0000

    update

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,16 +7,16 @@
 
 
     <div class="w-[6rem] ml-[10rem] mt-5  text-center p-2 shadow rounded">
-        <a href="{{route('dashboard')}}">
+        <a href="{{route('admin.home')}}">
             <span><i class="fa-solid fa-arrow-right-from-bracket fa-rotate-180 "></i>&nbsp;<spanclear>Back</span></span>
         </a>
     </div>
 
-   <div class="m-auto w-[50rem] mt-5">
-     <x-auth-session-status class="mb-5" :status="session('status')" />
-    <x-success-message class="mb-5" :success="session('success')" />
-    <x-error-message class="mb-5" :fail="session('fail')" />
-   </div>
+    <div class="m-auto w-[50rem] mt-5">
+        <x-auth-session-status class="mb-5" :status="session('status')" />
+        <x-success-message class="mb-5" :success="session('success')" />
+        <x-error-message class="mb-5" :fail="session('fail')" />
+    </div>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
