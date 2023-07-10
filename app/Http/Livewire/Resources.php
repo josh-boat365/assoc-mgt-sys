7f5742d538748db4a2159a155d70a4056a7d2a1a
josh-boat365 <nyarko.boateng20@outlook.com>
2023-07-10 09:13:55 +0000

    update: livewire powergrid table for resources

---
+++ app/Http/Livewire/Resources.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Resources;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use PowerComponents\LivewirePowerGrid\Filters\Filter;
+use PowerComponents\LivewirePowerGrid\Rules\{Rule, RuleActions};
+use PowerComponents\LivewirePowerGrid\Traits\{ActionButton, WithExport};
+use PowerComponents\LivewirePowerGrid\{Button, Column, Exportable, Footer, Header, PowerGrid, PowerGridComponent, PowerGridColumns};
+use OpenSpout\Writer\XLSX\Options;
+
+
+final class Users extends PowerGridComponent
+{
+    use ActionButton;
+    use WithExport;
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Features Setup
+    |--------------------------------------------------------------------------
+    | Setup Table's general features
+    |
+    */
+    public function setUp(): array
+    {
+        $this->showCheckBox();
+
+        return [
+            Exportable::make('export')
+                ->striped('#12rrrr')
+                ->type(Exportable::TYPE_XLS, Exportable::TYPE_CSV),
+            Header::make()->showSearchInput(),
+            Footer::make()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Datasource
+    |--------------------------------------------------------------------------
+    | Provides data to your Table using a Model or Collection
+    |
+    */
+
+    /**
+     * PowerGrid datasource.
+     *
+     * @return Builder<\App\Models\User>
+     */
+    public function datasource(): Builder
+    {
+        return Resources::query();
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Relationship Search
+    |--------------------------------------------------------------------------
+    | Configure here relationships to be used by the Search and Table Filters.
+    |
+    */
+
+    /**
+     * Relationship search.
+     *
+     * @return array<string, array<int, string>>
+     */
+    public function relationSearch(): array
+    {
+        return [];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Add Column
+    |--------------------------------------------------------------------------
+    | Make Datasource fields available to be used as columns.
+    | You can pass a closure to transform/modify the data.
+    |
+    | ❗ IMPORTANT: When using closures, you must escape any value coming from
+    |    the database using the `e()` Laravel Helper function.
+    |
+    */
+    public function addColumns(): PowerGridColumns
+    {
+        return PowerGrid::columns()
+            ->addColumn('id')
+            ->addColumn('title')
+
+            /** Example of custom column using a closure **/
+            ->addColumn('title_lower', fn (Resources $model) => strtolower(e($model->title)))
+
+            ->addColumn('description')
+            ->addColumn('link')
+
+            ->addColumn('created_at_formatted', fn (Resources $model) => Carbon::parse($model->created_at)->format('d/m/Y H:i:s'));
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Include Columns
+    |--------------------------------------------------------------------------
+    | Include the columns added columns, making them visible on the Table.
+    | Each column can be configured with properties, filters, actions...
+    |
+    */
+
+    /**
+     * PowerGrid Columns.
+     *
+     * @return array<int, Column>
+     */
+    public function columns(): array
+    {
+        return [
+            Column::make('Id', 'id'),
+            Column::make('Title', 'title')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Description', 'description')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Link', 'link')
+                ->sortable()
+                ->searchable(),
+
+
+        ];
+    }
+
+    /**
+     * PowerGrid Filters.
+     *
+     * @return array<int, Filter>
+     */
+    public function filters(): array
+    {
+        return [
+            Filter::inputText('title')->operators(['contains']),
+            Filter::inputText('description')->operators(['contains']),
+            Filter::inputText('link')->operators(['contains']),
+            Filter::datetimepicker('created_at'),
+        ];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Method
+    |--------------------------------------------------------------------------
+    | Enable the method below only if the Routes below are defined in your app.
+    |
+    */
+
+    /**
+     * PowerGrid User Action Buttons.
+     *
+     * @return array<int, Button>
+     */
+
+
+    public function actions(): array
+    {
+        return [
+            Button::make('edit', 'Edit')
+                ->class('bg-green-500 cursor-pointer text-white px-3 py-2.5 m-1 rounded text-sm')
+                ->route('', function (\App\Models\User $model) {
+                    return $model->id;
+                }),
+
+            Button::make('role', 'Role')
+                ->class('bg-green-500 cursor-pointer text-white px-3 py-2.5 m-1 rounded text-sm')
+                ->route('', function (\App\Models\User $model) {
+                    return $model->id;
+                }),
+
+
+            Button::make('reset', 'Reset')
+                ->class('bg-green-500 cursor-pointer text-white px-3 py-2.5 m-1 rounded text-sm')
+                ->route('', function (\App\Models\User $model) {
+                    return $model->id;
+                }),
+
+
+            Button::make('destroy', 'Suspend')
+                ->class('bg-red-500 cursor-pointer text-white px-3 py-2 m-1 rounded text-sm')
+                ->route('', function (\App\Models\User $model) {
+                    return $model->id;
+                })
+                ->method('delete')
+        ];
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Rules
+    |--------------------------------------------------------------------------
+    | Enable the method below to configure Rules for your Table and Action Buttons.
+    |
+    */
+
+    /**
+     * PowerGrid User Action Rules.
+     *
+     * @return array<int, RuleActions>
+     */
+
+    /*
+    public function actionRules(): array
+    {
+       return [
+
+           //Hide button edit for ID 1
+            Rule::button('edit')
+                ->when(fn($user) => $user->id === 1)
+                ->hide(),
+        ];
+    }
+    */
+}
